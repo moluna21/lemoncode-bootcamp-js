3fefb1cc1db894ae894649f2d5deb96fe9e64153
moluna21 <conchamarina.luna@gmail.com>
2022-08-15 16:55:50 +0200

    * Update JS code lines

--- 04-funciones-eventos/laboratorio-1/script.js
+++ 04-funciones-eventos/laboratorio-1/script.js
@@ -6,7 +6,6 @@ var divideValues = () => getFirstValue() / getSecondValue();
 var sumValues = () => getFirstValue() + getSecondValue();
 var restValues = () => getFirstValue() - getSecondValue();
 
-// isNan -> es una función que lo que hace es comprobar si es un nº
 var setResult = (result) => {
   isNaN(getFirstValue()) || isNaN(getSecondValue()) ? document.getElementById("print-result").innerHTML = 'Error' : document.getElementById("print-result").innerHTML = result;
 }
@@ -23,14 +22,3 @@ document.getElementById("button-sum").addEventListener("click", function() {
 document.getElementById("button-rest").addEventListener("click", function() {
   setResult(restValues())
 });
-
-
-// SUMAS en JS
-// Si los 2 números vienen recogidos por las variables X e Y,
-// al poner x+y lo que hará es concatenar sus valores.
-// Si los valores son x=2, y=3, el resultado de sumar x+y no será 5,
-// será 23, ya que concatena. Para evitar ese problema se ha de usar:
-// parseInt:  si se trata de números enteros
-// parseFloat: si se trata de números con decimales
-// En este caso para sumar x=2, y=3 usaremos la siguiente expresión.
-//    parseInt(x)+parseInt(y)
